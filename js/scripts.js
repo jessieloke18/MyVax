23e496dc5f2eac08a89c32d03bbde9cbe401a63a
jessieloke18 <49142246+jessieloke18@users.noreply.github.com>
2021-09-29 13:51:35 +0800

    added function and alert msg to allow patient to select another date if it is after the expiration date

--- js/scripts.js
+++ js/scripts.js
@@ -89,3 +89,26 @@ function loginRedirect(){
     document.getElementById("redirect-login").href="javascript:history.back()"
     
 }
+
+//compare dates
+function compareDates(){
+
+    var dateParts;
+
+    var batchExpiryDate = document.getElementById("batch-expiry-date").getAttribute("data-value");
+ 
+    var selectedDate = document.getElementById("datepicker").value;
+
+    dateParts= batchExpiryDate.split("/");
+    batchExpiryDate = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+
+    dateParts = selectedDate.split("/");
+    selectedDate = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+
+    if(selectedDate>batchExpiryDate){
+        document.getElementById("date-alert").style.display="block";
+    }
+       
+    else
+        window.location.href = "afterbooking.html";
+}
