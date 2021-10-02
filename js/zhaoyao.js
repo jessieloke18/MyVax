39e5f4377a8da6603afd4a61967ec86d14cd9a1a
EdwardONeon <edwardoneon@outlook.com>
2021-10-02 10:41:24 +0800

    touch up navbar and calendar in record batch page

---
+++ js/zhaoyao.js
@@ -0,0 +1,10 @@
+//datepicker
+
+$('#datepicker').datepicker({
+    weekStart: 1,
+    daysOfWeekHighlighted: "6,0",
+    autoclose: true,
+    todayHighlight: true,
+});
+
+$('#datepicker').datepicker("setDate", new Date());
