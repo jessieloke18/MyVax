8f0b958e991178d25ccb2d6fbe7611a69c7bd307
EdwardONeon <edwardoneon@outlook.com>
2021-10-09 12:15:18 +0800

    added database connection config file

---
+++ dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbName = "myvax";
+
+    $conn = mysqli_connect($servername,$username,$password,$dbName);
+?>
