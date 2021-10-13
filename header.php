431959566d5928901dd1414e370cd3037b2e98f7
jessieloke18 <49142246+jessieloke18@users.noreply.github.com>
2021-10-13 11:02:58 +0800

    fixed navbar dropdown not showing up in mobile view for logged in users issue by separating the nav bars

--- header.php
+++ header.php
@@ -12,23 +12,23 @@ include 'dbconnect.php';
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <!-- Datepicker -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.min.css" rel="stylesheet" />
 </head>
 
 <body>
-  <!--Navigation bar-->
-  <nav class="navbar navbar-expand-lg navbar-light sticky-top">
-    <div class="container">
-      <a class="navbar-brand" href="index.php"><img src="images/logowhite1.png" alt="MyVax Logo" width="110" height="50"> </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <i class="fas fa-bars"></i>
-      </button>
+
   <!--If username session is not set, use these nav links-->
-      <?php
-      if (!isset($_SESSION['username'])) { ?>
+  <?php
+  if (!isset($_SESSION['username'])) { ?>
+    <!--Navigation bar-->
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="images/logowhite1.png" alt="MyVax Logo" width="110" height="50"> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav ml-auto">
             <a class="nav-link" href="index.php">Home</a>
@@ -36,14 +36,24 @@ include 'dbconnect.php';
             <a class="nav-link" href="login.php">Login</a>
           </div>
         </div>
- <!--If username session is set, use these nav links-->
-      <?php } else { ?>
+      </div>
+    </nav>
+    <!--If username session is set, use these nav links-->
+  <?php } else { ?>
+
+    <!--Navigation bar-->
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="images/logowhite1.png" alt="MyVax Logo" width="110" height="50"> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
               <a class="nav-link" href="index.php">Home</a>
             </li>
-            </li>
+
             <?php
             $username = $_SESSION['username'];
             $result = mysqli_query($conn, "SELECT* FROM user WHERE username='$username'");
@@ -72,6 +82,8 @@ include 'dbconnect.php';
             </li>
           </ul>
         </div>
-      <?php } ?>
-    </div>
-  </nav>
+
+      </div>
+    </nav>
+
+  <?php }
