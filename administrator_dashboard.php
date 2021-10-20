541eefabfbc7dde8d55b0f92d10af7f9878cd71d
EdwardONeon <edwardoneon@outlook.com>
2021-10-20 16:27:12 +0800

    updated link address in all pages, added view vaccine batch info page

--- administrator_dashboard.php
+++ administrator_dashboard.php
@@ -13,133 +13,129 @@ $row = mysqli_fetch_assoc($result);
 $centreName =  $row['centreName'];
 $_SESSION["centreName"] = $centreName;
 ?>
-    <div class="container" id="admin-alert">
-        <div class="alert alert-info" role="alert">
-            Hi Administrator <?php echo $username ?>. You are now managing: <?php echo $centreName ?>.
+<div class="container" id="admin-alert">
+    <div class="alert alert-info" role="alert">
+        Hi Administrator <?php echo $username ?>. You are now managing: <?php echo $centreName ?>.
+    </div>
+</div>
+
+<div class="container d-flex align-items-center" id="admin-functions" onmouseover="boldCardText()">
+    <div class="row">
+        <div class="col-6 col-lg-3 mb-3">
+            <a href="record_new_vaccine_batch.php" class="card-links">
+                <div class="card function-card h-100 rounded-0">
+                    <img src="images/function4.jpg" class="card-img-top" alt="Record Vaccine Batch">
+                    <div class="card-body">
+                        <p class="card-text">
+                            Record Vaccine Batch
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-6 col-lg-3 mb-3">
+            <a href="view_vaccine_batch_info.php" class="card-links">
+                <div class="card function-card h-100 rounded-0">
+                    <img src="images/function3.jpg" class="card-img-top" alt="View Batch Info">
+                    <div class="card-body">
+                        <p class="card-text">
+                            View Batch Info
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-6 col-lg-3 mb-3">
+            <a href="#" class="card-links">
+                <div class="card function-card h-100 rounded-0">
+                    <img src="images/editprofile.jpg" class="card-img-top" alt="Edit Profile">
+                    <div class="card-body">
+                        <p class="card-text">
+                            Manage Centre
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-6 col-lg-3 mb-3">
+            <a href="#" class="card-links">
+                <div class="card function-card h-100 rounded-0">
+                    <img src="images/managepatients.jpg" class="card-img-top" alt="Logout">
+                    <div class="card-body">
+                        <p class="card-text">
+                            Manage Patients
+                        </p>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
-    
-    <div class="container d-flex align-items-center" id="admin-functions" onmouseover="boldCardText()">
-        <div class="row">
-            <div class="col-6 col-lg-3 mb-3">
-                <a href="record_new_vaccine_batch.php" class="card-links">
-                    <div class="card function-card h-100 rounded-0">
-                        <img src="images/function4.jpg" class="card-img-top" alt="Record Vaccine Batch">
-                        <div class="card-body">
-                            <p class="card-text">
-                                Record Vaccine Batch
-                            </p>
-                        </div>
-                    </div>
-                </a>
-            </div>
+</div>
 
-            <div class="col-6 col-lg-3 mb-3">
-                <a href="view_vaccine_batch_info.html" class="card-links">
-                    <div class="card function-card h-100 rounded-0">
-                        <img src="images/function3.jpg" class="card-img-top" alt="View Batch Info">
-                        <div class="card-body">
-                            <p class="card-text">
-                                View Batch Info
-                            </p>
-                        </div>
-                    </div>
-                </a>
-            </div>
+<div class="container mb-3" style="width:80%">
+    <h5>Overview</h5>
+</div>
 
-            <div class="col-6 col-lg-3 mb-3">
-                <a href="#" class="card-links">
-                    <div class="card function-card h-100 rounded-0">
-                        <img src="images/editprofile.jpg" class="card-img-top" alt="Edit Profile">
-                        <div class="card-body">
-                            <p class="card-text">
-                                Manage Centre
-                            </p>
-                        </div>
+<div class="container mb-5" style="width:80%">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card overview-card bg-success">
+                <div class="card-body d-flex flex-column">
+                    <div class="ml-auto">
+                        <i class="fas fa-syringe syringe-admin fa-2x" style="color:white !important; padding:0"></i>
                     </div>
-                </a>
-            </div>
-
-            <div class="col-6 col-lg-3 mb-3">
-                <a href="#" class="card-links">
-                    <div class="card function-card h-100 rounded-0">
-                        <img src="images/managepatients.jpg" class="card-img-top" alt="Logout">
-                        <div class="card-body">
-                            <p class="card-text">
-                                Manage Patients
-                            </p>
-                        </div>
+                    <div class="mt-auto">
+                        <p class="card-text" id="overview-text">Vaccinations <span class="badge badge-light badge-pill">110</span></p>
                     </div>
-                </a>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="container mb-3" style="width:80%">
-        <h5>Overview</h5>
-    </div>
-
-    <div class="container mb-5" style="width:80%">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card overview-card bg-success">
-                    <div class="card-body d-flex flex-column">
-                        <div class="ml-auto">
-                            <i class="fas fa-syringe syringe-admin fa-2x" style="color:white !important; padding:0"></i>
-                        </div>
-                        <div class="mt-auto">
-                            <p class="card-text" id="overview-text">Vaccinations <span
-                                    class="badge badge-light badge-pill">110</span></p>
-                        </div>
+        <div class="col-md-3">
+            <div class="card overview-card bg-warning">
+                <div class="card-body d-flex flex-column">
+                    <div class="ml-auto">
+                        <i class="far fa-list-alt fa-2x" style="color:white"></i>
+                    </div>
+                    <div class="mt-auto">
+                        <p class="card-text" id="overview-text">Pending <span class="badge badge-light badge-pill">20</span></p>
                     </div>
                 </div>
             </div>
-
-            <div class="col-md-3">
-                <div class="card overview-card bg-warning">
-                    <div class="card-body d-flex flex-column">
-                        <div class="ml-auto">
-                            <i class="far fa-list-alt fa-2x" style="color:white"></i>
-                        </div>
-                        <div class="mt-auto">
-                            <p class="card-text" id="overview-text">Pending <span
-                                    class="badge badge-light badge-pill">20</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="card overview-card bg-danger">
-                    <div class="card-body d-flex flex-column">
-                        <div class="ml-auto">
-                            <i class="far fa-check-circle fa-2x" style="color:white"></i>
-                        </div>
-                        <div class="mt-auto">
-                            <p class="card-text" id="overview-text">Administered <span
-                                    class="badge badge-light badge-pill">89</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="card overview-card bg-primary">
-                    <div class="card-body d-flex flex-column">
-                        <div class="ml-auto">
-                            <i class="fas fa-users fa-2x"></i>
-                        </div>
-                        <div class="mt-auto">
-                            <p class="card-text" id="overview-text">Users <span
-                                    class="badge badge-light badge-pill">31</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         </div>
-    </div>
 
-    <!--Footer-->
+        <div class="col-md-3">
+            <div class="card overview-card bg-danger">
+                <div class="card-body d-flex flex-column">
+                    <div class="ml-auto">
+                        <i class="far fa-check-circle fa-2x" style="color:white"></i>
+                    </div>
+                    <div class="mt-auto">
+                        <p class="card-text" id="overview-text">Administered <span class="badge badge-light badge-pill">89</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card overview-card bg-primary">
+                <div class="card-body d-flex flex-column">
+                    <div class="ml-auto">
+                        <i class="fas fa-users fa-2x"></i>
+                    </div>
+                    <div class="mt-auto">
+                        <p class="card-text" id="overview-text">Users <span class="badge badge-light badge-pill">31</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!--Footer-->
 <?php include 'footer.php'; ?>
 <script src="js/zhaoyao.js"></script>
---
+++ view_vaccine_batch_info.php
@@ -0,0 +1,53 @@
+<!--Header and dbconnect-->
+<?php
+session_start();
+include 'dbconnect.php';
+include 'header.php';
+?>
+<title>View Vaccine Batch Info</title>
+<link rel="stylesheet" href="css/zhaoyao.css">
+
+
+<h2 class="mt-5">Available Vaccine Batches</h2>
+<!-- Table to show the batches in the current healthcare centre -->
+<div class="container table-section mt-5">
+  <table class="table table-bordered">
+    <thead class="thead-light">
+      <tr>
+        <th scope="col">BatchNo</th>
+        <th scope="col">Vaccine Name</th>
+        <th scope="col">Pending</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+    $username = $_SESSION['username'];
+    $centreName = $_SESSION['centreName'];
+    $statement1 = "SELECT t1.batchNo, t2.vaccineName, t1.quantityPending FROM batch AS t1 INNER JOIN vaccine AS t2 ON t1.vaccineID = t2.vaccineID WHERE centreName='$centreName'";
+    $result = mysqli_query($conn, $statement1);
+    if (mysqli_num_rows($result) > 0){
+      while($row = mysqli_fetch_assoc($result)){
+        echo '<tr><th scope="row">' . $row['batchNo'] . "</th><td>" . $row['vaccineName'] . "</td><td>" . $row['quantityPending'] . "</td>" 
+        .'<td><a href="view_vaccination_info.html"><i class="far fa-eye"></i></a></td></tr>';
+      }
+    } else{
+      echo '<tr><td colspan="4">There is no batch in this healthcare centre yet.</td></tr>';
+    }
+    ?>
+    </tbody>
+  </table>
+</div>
+
+<div class="container d-flex justify-content-center mb-5 mt-5">
+  <a href="administrator_dashboard.php"><button class="btn button-pcvs btn-info">Back To Dashboard</button></a>
+
+</div>
+
+
+
+
+
+<!--Footer-->
+<?php include 'footer.php'; ?>
+<script src="../js/zhaoyao.js"></script>
