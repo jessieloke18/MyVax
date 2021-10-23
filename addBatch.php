773e16e00b90becff96b8b2dc19d0583702f61d8
EdwardONeon <edwardoneon@outlook.com>
2021-10-23 23:34:45 +0800

    modified success message of add batch

--- addBatch.php
+++ addBatch.php
@@ -20,7 +20,7 @@ if (isset($_POST['submit'])) {
 
         $query_run = mysqli_multi_query($conn, $sql);
         if ($query_run) {
-            echo '<script>alert("Successful");</script>';
+            echo '<script>alert("Batch is added successfully.");</script>';
             echo "<script>window.location.href='administrator_dashboard.php';</script>";
         } else {
             echo '<script>alert("Unsuccessful");</script>';
