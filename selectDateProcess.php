281f8ac4f4303da3a5376a2c58076b237c68b31d
jessieloke18 <49142246+jessieloke18@users.noreply.github.com>
2021-10-23 21:21:25 +0800

    added else statement and comments to php script

--- selectDateProcess.php
+++ selectDateProcess.php
@@ -25,19 +25,24 @@ if (isset($_POST['appointment-submit'])) {
 
         $query_run = mysqli_query($conn, $query);
         if ($query_run) {
-            $sql = "SELECT vaccinationID FROM vaccination WHERE username ='$username' AND appointmentDate ='$newAppDate'";
-            $result = mysqli_query($conn, $sql);
-            $row = mysqli_fetch_assoc($result);
+            //retrieve and store vaccinationID in session
+            $query = "SELECT vaccinationID FROM vaccination WHERE username ='$username' AND appointmentDate ='$newAppDate'";
+            $query_run = mysqli_query($conn, $query);
+            $row = mysqli_fetch_assoc($query_run);
             $_SESSION['vaccinationID'] = $row["vaccinationID"];
 
+            //update quantityAvailable and quantityPending
             $query = "UPDATE batch SET quantityAvailable = quantityAvailable-1, quantityPending = quantityPending+1 WHERE batchNo = '$batchNo'";
             $query_run = mysqli_query($conn, $query);
 
             if ($query_run) {
                 echo "<script>window.location.href='bookingSuccess.php?" . $_SESSION['vaccinationID'] . "'</script>";
+                
             } else {
                 echo '<script>alert("Unsuccessful");</script>';
             }
+        } else {
+            echo '<script>alert("Unsuccessful");</script>';
         }
     }
 }
