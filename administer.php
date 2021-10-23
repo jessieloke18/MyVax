f5ec1a17794c78f085a2a4abc5c31674593e10d9
EdwardONeon <edwardoneon@outlook.com>
2021-10-24 00:02:33 +0800

    updated quantityAdministered when log as administered

--- administer.php
+++ administer.php
@@ -6,8 +6,9 @@ if (isset($_POST['submit'])) {
     $vaccinationID = $_SESSION["vaccinationID"];
     $remarks = $_POST['remarks'];
     $batchNo = $_SESSION['batchNo'];
-    $sql = "UPDATE vaccination SET remarks = '$remarks', status = 'administered' WHERE vaccinationID = '$vaccinationID'";
-    $query_run = mysqli_query($conn, $sql);
+    $sql = "UPDATE vaccination SET remarks = '$remarks', status = 'administered' WHERE vaccinationID = '$vaccinationID';";
+    $sql .= "UPDATE batch SET quantityAdministered = quantityAdministered + 1 WHERE batchNo = '$batchNo';";
+    $query_run = mysqli_multi_query($conn, $sql);
     if ($query_run) {
         echo '<script>alert("Vaccination administered successfully!");</script>';
         echo '<script>window.location.href="view_vaccination_info.php?batchNo=' . $batchNo . '";</script>';
